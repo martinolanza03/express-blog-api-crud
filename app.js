de4035529363ee884bb9b4b5186b7899f063d08c
martinolanza03 <lanzamartino3@gmail.com>
2025-03-28 15:37:25 +0100

    feat: add call to postRouter

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const postsRouter = require('./routers/postRouter.js');
 
 //Static asset
 app.use(express.static('public'));
+
+//Call routes in postsRouter.js
+app.use('/posts', postsRouter);
 
 //Server port
 app.listen(port, () => {
