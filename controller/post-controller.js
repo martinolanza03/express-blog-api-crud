75a312a24544c1fc2e6f107346d69fa8990c6177
martinolanza03 <lanzamartino3@gmail.com>
2025-03-28 15:21:48 +0100

    feat: create a controller

---
+++ controller/post-controller.js
@@ -0,0 +1,28 @@
+const posts = require('../posts.js');
+
+function index(req, res) {
+    res.json(posts);
+}
+
+function show(req, res) {
+    res.send(`Dettagli del posts ${req.params.id}`);
+}
+function store(req, res) {
+    res.send(`Creare un nuovo elemento`);
+}
+function update(req, res) {
+    res.send(`Modifica integrale dell'elemento con id ${req.params.id}`);
+}
+
+function patch(req, res) {
+    res.send(`Modifica parziale dell'elemento con id ${req.params.id}`);
+}
+
+
+function destroy(req, res) {
+    res.send(`Eliminazione dell'elemento con id ${req.params.id}`);
+}
+
+
+// export all function
+module.exports = { index, show, store, update, destroy }
