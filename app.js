668ff54f28298e9dcc6b86d931c7805d15570a7c
martinolanza03 <lanzamartino3@gmail.com>
2025-04-01 12:37:57 +0200

    feat: add error in the app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const postsRouter = require('./routers/postRouter.js');
+const errorsHandler = require('./middleware/errorHandler.js')
 
 //Static asset
 app.use(express.static('public'));
@@ -11,6 +12,9 @@ app.use(express.json());
 
 //Call routes in postsRouter.js
 app.use('/posts', postsRouter);
+
+//Call the middleware
+app.use(errorsHandler);
 
 //Server port
 app.listen(port, () => {
